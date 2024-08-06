07a032a19cd34c6f81e090cb8e63bdeb52a6f42a
hubjoon <blobal@naver.com>
2024-08-07 00:44:46 +0900

    Yongin1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <img src="./사진.jpg" alt="M.int icon" className="mint-icon" />
+        <div className="header-text">
+          <h1>M.int</h1>
+          <p>올바른 복지를 말하다.</p>
+        </div>
       </header>
+      <main className="main-content">
+        <img src="./사진.jpg" alt="Large M.int icon" className="large-mint-icon" />
+        <h2>민트에 오신 것을 환영합니다!</h2>
+        <p>민트는 용인시 노인분들과 함께하는<br /> 노인 복지플랫폼 입니다.</p>
+        <p className="search-instruction">검색어를 통해 AI 서비스를 이용</p>
+        <input type="text" placeholder="검색어를 입력해주세요" className="search-input" />
+      </main>
     </div>
   );
 }
